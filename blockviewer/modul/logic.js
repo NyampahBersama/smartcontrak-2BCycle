55041d08183b8466c6e99f0b19186570cfb0c57c
BangNyampahin <nyampahbersama6@gmail.com>
2025-06-28 07:52:04 +0700

    Create logic.js

---
+++ blockviewer/modul/logic.js
@@ -0,0 +1,4 @@
+// Contoh: Bisa isi perintah, animasi, utilities, dll
+console.log("Custom logic.js loaded!");
+// Kamu bisa override addMessage atau fungsi lain jika butuh "pena" jenis baru
+// Atau masukkan interaksi lain, trigger animasi dsb.
